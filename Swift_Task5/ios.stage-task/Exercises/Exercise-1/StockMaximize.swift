076b160ff983bd4222e0cffe04dfbe2057e417bd
Vlad Burak <byrakvlad@gmail.com>
2021-06-14 18:24:24 +0300

    init:first commit

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,21 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if (prices.isEmpty) {
+            return 0
+        }
+     
+        var expectedProfit = 0
+        for i in 0..<prices.count {
+            var prof = 0
+            for j in i+1..<prices.count {
+                if (prices[j] - prices[i] > prof) {
+                    prof = prices[j] - prices[i]
+                    
+                }
+            }
+            expectedProfit += prof
+        }
+        return expectedProfit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,13 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
+        
+        if ( message.count >= 1 && message.count <= 60 ) {
+                    return ""
+                }
+        
+        
+        
         return ""
     }
 }
